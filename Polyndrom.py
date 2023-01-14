1efe8de930466d8041b9a4d7d1986f9f05a8a40b
Shinodegu <118846679+shinodegu@users.noreply.github.com>
2023-01-14 19:19:42 +0200

    Update Polyndrom.py

--- Polyndrom.py
+++ Polyndrom.py
@@ -1,4 +1,4 @@
 inputdata = ['Страна', 'шалаш', 'Летел', 'вертолет', 'УЧУ', 'мэм', 'язык']
 
-b = list(filter(lambda word: word != word[::-1], inputdata))
+b = list(filter(lambda word: word.upper() != word.upper()[::-1], inputdata))
 print(b)
